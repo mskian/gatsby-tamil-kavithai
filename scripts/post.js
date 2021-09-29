79905019b8adcfd34bc1a9c0ceaac6cbc9e402f5
mskian <veerfun23@gmail.com>
2021-09-29 15:57:38 +0530

    📔 Rewrite New Post Script

--- scripts/post.js
+++ scripts/post.js
@@ -1,21 +1,26 @@
 const fs = require("fs")
-const prompt = require("prompt-sync")()
 
-const title = prompt("Enter the Post Title: ")
+function getCurrentDate(n) {
+  return (n < 10 ? "0" : "") + n
+}
+
+var date = new Date()
+var month = getCurrentDate(date.getMonth() + 1)
+var day = getCurrentDate(date.getDate())
+var year = date.getFullYear()
+var formattedDate = year + "-" + month + "-" + day
+
 const blogdir = "./kavithai"
 
-if (!title && !postdata) {
-  console.log("Please specify a post title.")
-  return
-}
 const random_id = Math.floor(1000 + Math.random() * 9000)
 const basename = "tamil-kavithai-" + random_id
 
 const contents = `---
-title: "${title}"
+title: ""
 description: ""
-categories: ["kavithai"]
-date: ""
+categories: [""]
+date: "${formattedDate}"
+copied: ""
 published: false
 ---
 `
